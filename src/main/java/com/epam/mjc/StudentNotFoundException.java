598ecdade9931092edea7f98204f76ae53dd3e3f
murod <murad.djanikulov@gmail.com>
2024-02-23 14:27:31 +0500

    task2 done

---
+++ src/main/java/com/epam/mjc/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.epam.mjc;
+
+public class StudentNotFoundException extends IllegalArgumentException {
+
+    public StudentNotFoundException(long studentId) {
+        super("Could not find student with ID " + studentId);
+    }
+}
